918685c33cc41127f96b6b51e71f2e0bd7fc3600
Berkay Derin <68425051+zewodi@users.noreply.github.com>
2022-01-05 00:32:51 +0300

    Update main.java

--- basamakToplami/main.java
+++ basamakToplami/main.java
@@ -8,8 +8,7 @@ public class BasamakSayilariniToplama {
         int sonuc = 0;
         String numString = String.valueOf(sayi);
         for (int i = 0; i < numString.length(); i++) {
-            sonuc = sonuc + Integer.parseInt(String.valueOf(numString.charAt(i)));
-            
+            sonuc = sonuc + Integer.parseInt(String.valueOf(numString.charAt(i)));    
         }
         System.out.println("Basamakların toplamı:" + sonuc);
     }
